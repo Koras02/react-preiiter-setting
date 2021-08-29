6f1bac602d4ff7e1d7a190d924aba78e3217ecc6
Koras02 <wjdgh0727@gmail.com>
2021-08-29 20:36:36 +0900

    prettier 세팅법

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
+/* eslint-disable jsx-a11y/alt-text */
+import React from 'react';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import './App.css';
+import Slider from 'react-slick';
 
 function App() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 2000,
+    autoplaySpeed: 2000,
+    cssEase: 'linear',
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="app">
+        <div>
+          <div className="App">코드 정렬</div>
+          <div className="Slider">
+            <Slider {...settings}>
+              <div className="Slider-img">
+                <img
+                  className="Slider-img"
+                  src="https://mblogthumb-phinf.pstatic.net/MjAxODEyMDVfMTgg/MDAxNTQ0MDA3NDgyNjM3._OVZI1LYTYdyEI2qKPC47MJLT9UhVUadypz6a8g6Xcsg.8pCkwEiQLtxYvCFRR-F4F5r3_fM0GUjn5B8zV3FMPhkg.GIF.nang723/IMG_0838.GIF?type=w800"
+                />
+              </div>
+              <div className="Slider-img">
+                <img
+                  className="Slider-img"
+                  src="https://mblogthumb-phinf.pstatic.net/MjAxODEyMDVfMTgg/MDAxNTQ0MDA3NDgyNjM3._OVZI1LYTYdyEI2qKPC47MJLT9UhVUadypz6a8g6Xcsg.8pCkwEiQLtxYvCFRR-F4F5r3_fM0GUjn5B8zV3FMPhkg.GIF.nang723/IMG_0838.GIF?type=w800"
+                />
+              </div>{' '}
+              <div className="Slider-img">
+                <img
+                  className="Slider-img"
+                  src="https://mblogthumb-phinf.pstatic.net/MjAxODEyMDVfMTgg/MDAxNTQ0MDA3NDgyNjM3._OVZI1LYTYdyEI2qKPC47MJLT9UhVUadypz6a8g6Xcsg.8pCkwEiQLtxYvCFRR-F4F5r3_fM0GUjn5B8zV3FMPhkg.GIF.nang723/IMG_0838.GIF?type=w800"
+                />
+              </div>{' '}
+              <div className="Slider-img">
+                <img
+                  className="Slider-img"
+                  src="https://mblogthumb-phinf.pstatic.net/MjAxODEyMDVfMTgg/MDAxNTQ0MDA3NDgyNjM3._OVZI1LYTYdyEI2qKPC47MJLT9UhVUadypz6a8g6Xcsg.8pCkwEiQLtxYvCFRR-F4F5r3_fM0GUjn5B8zV3FMPhkg.GIF.nang723/IMG_0838.GIF?type=w800"
+                />
+              </div>
+            </Slider>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
